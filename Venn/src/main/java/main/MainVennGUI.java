a14a05564a4d682edd21c46234ee8685a1c10d10
Chidalu Agbakwa <cagbakwa@gmail.com>
2020-01-09 19:08:25 -0500

    Added my name

--- Venn/src/main/java/main/MainVennGUI.java
+++ Venn/src/main/java/main/MainVennGUI.java
@@ -1,32 +1,45 @@
 package main;
 
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+
 import java.awt.Font;
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.EventQueue;
+import javax.swing.JButton;
 
 public class MainVennGUI extends JFrame {
+	
+	
 	public MainVennGUI() {
 		getContentPane().setLayout(null);
+		setBounds(1000,1000,1245,731);
+		setLocationRelativeTo(null);
 		
 		JLabel mainTitle = new JLabel("Venn Diagram Maker");
 		mainTitle.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		mainTitle.setBounds(446, 13, 402, 92);
 		getContentPane().add(mainTitle);
 		
-		Canvas canvas = new Canvas();
-		canvas.setBackground(Color.WHITE);
-		canvas.setBounds(238, 100, 676, 455);
-		getContentPane().add(canvas);
+		JButton btnNewButton = new JButton("Chidalu Agbakwa");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton.setBounds(95, 105, 307, 80);
+		getContentPane().add(btnNewButton);
 	}
 	
 	public static void main(String[] args) {
-		try {
-			JFrame test = new MainVennGUI();
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrame window = new MainVennGUI();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
