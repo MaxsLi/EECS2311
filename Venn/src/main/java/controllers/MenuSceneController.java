c188c26bcb834dd24a279f65133b15db8b4a0393
Chidalu Agbakwa <cagbakwa@gmail.com>
2020-01-21 16:59:01 -0500

    Created two Paackages and Controller classes

---
+++ Venn/src/main/java/controllers/MenuSceneController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuSceneController {
+	
+	@FXML
+	private Button createNewBttn;
+	
+	@FXML
+	private Button getExistingBttn;
+	
+	public MenuSceneController() {
+		
+	}
+	
+
+}
