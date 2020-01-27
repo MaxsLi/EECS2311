ee1438606dc837faba35c8e85fc7733a119a8c25
Shangru(Max) Li <maxsli@protonmail.com>
2020-01-27 11:50:19 -0500

    Small fix

--- Venn/src/main/java/application/MainApp.java
+++ Venn/src/main/java/application/MainApp.java
@@ -46,7 +46,7 @@ public class MainApp extends Application {
 	
 	private void loadRootLayout() throws IOException {
 		this.loader = new FXMLLoader();
-		this.loader.setLocation(getClass().getResource("rootLayout.fxml")); 		
+		this.loader.setLocation(getClass().getResource("RootLayout.fxml")); 		
 		this.rootLayout = (BorderPane) loader.load();
 	}
 	
