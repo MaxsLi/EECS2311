c1ce94fe79edc1aff3c508bb287f106e1190df0d
rsuwary <rsuwary@my.yorku.ca>
2020-02-11 15:10:13 -0500

    started on loadTest

--- Venn/src/test/java/ShapeSceneContTest.java
+++ Venn/src/test/java/ShapeSceneContTest.java
@@ -40,12 +40,38 @@ public class ShapeSceneContTest {
 			cont.saveVenn(cont.getTextFields());
 			FileReader fr=new FileReader(System.getProperty("user.dir")+"\\src\\main\\java\\application\\save.csv");
 			BufferedReader br=new BufferedReader(fr);
-			String s;
-			for (int i = 0; i < expected.length; i++) {
-				s=br.readLine();
-				assertEquals(expected[i], s);
+			String[] s=new String[5];
+			for (int i = 0; i < s.length; i++) {
+				s[i]=br.readLine();
+				
 			}
 
+			assertArrayEquals(expected, s);
 		}
-		
+	
+		@Test
+		public void testLoad() {
+			String[] expected=new String[5];
+			TextField tf;
+			for (int i = 0; i < expected.length; i++) {
+				tf=new TextField();
+				tf.setText("I am test");
+				tf.setTranslateX(i);
+				tf.setTranslateY(i);
+				cont.getTextFields().add(tf);
+				expected[i]=tf.getText()+", "+tf.getTranslateX()+", "+tf.getTranslateY();
+				
+			}
+			cont.saveVenn(cont.getTextFields());
+			cont=new ShapeSceneController();
+			cont.loadVenn();
+			String[] s=new String[5];
+			
+			for (int i = 0; i < s.length; i++) {
+				tf=cont.getTextFields().get(i);
+				s[i]=tf.getText()+", "+tf.getTranslateX()+", "+tf.getTranslateY();
+				
+			}
+			assertArrayEquals(expected, s);
+		}
 }
