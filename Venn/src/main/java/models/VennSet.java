70e66cc822551a089ae9d4f1f22d25c6c7e09d4f
Shangru Li <maxsli@protonmail.com>
2020-02-21 17:03:30 -0500

    Refactored backend
    Added a VennShape class.

--- Venn/src/main/java/models/VennSet.java
+++ Venn/src/main/java/models/VennSet.java
@@ -1,16 +1,60 @@
 package models;
 
-import java.util.HashSet;
-import java.util.List;
+import javafx.geometry.Point2D;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
 
-public class VennSet extends HashSet<String> {
+import java.util.ArrayList;
 
-	public VennSet() {
-		super();
+public class VennSet extends ArrayList<TextField> {
+
+	private VennShape vennShape;
+
+	public VennSet(VennShape vennShape) {
+		this.vennShape = vennShape;
 	}
 
-	public VennSet(List<String> list) {
-		super(list);
+	public Location getLocation(TextField textField) throws Exception {
+		Circle leftCircle = (Circle)this.vennShape.getLeftShape();
+		Circle rightCircle = (Circle)this.vennShape.getRightShape();
+
+		Point2D leftCenter = leftCircle.localToParent(leftCircle.getCenterX(), leftCircle.getCenterY());
+		Point2D rightCenter = rightCircle.localToParent(rightCircle.getCenterX(), rightCircle.getCenterY());
+
+		double leftRadius = leftCircle.getRadius();
+		double rightRadius = rightCircle.getRadius();
+
+		Point2D textFieldLocation = textField.localToParent(textField.getScene().getX(),
+				textField.getScene().getY());
+
+		double distanceToLeft = textFieldLocation.distance(leftCenter);
+		double distanceToRight = textFieldLocation.distance(rightCenter);
+
+		/*
+		 * If TextField location is within radial distance of the left and right circle,
+		 * it must be somewhere in the intersection of the two circles
+		 */
+		if (distanceToLeft <= leftRadius && distanceToRight <= rightRadius) {
+			return Location.MIDDLE;
+		}
+		/*
+		 * Else if, if its within radial distance of the left Circle, it must be in the
+		 * left circle
+		 */
+		else if (distanceToLeft <= leftRadius) {
+			return Location.LEFT;
+		}
+		/*
+		 * Else if, if its within radial distance of the left Circle, it must be in the
+		 * right circle
+		 */
+		else if (distanceToRight <= rightRadius) {
+			return Location.RIGHT;
+		}
+
+		else {
+			throw new Exception("Invalid location");
+		}
 	}
 
 }
