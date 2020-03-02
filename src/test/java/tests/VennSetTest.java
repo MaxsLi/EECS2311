a15210b79aac752ef03e62561ce28449a6b1a1e3
Shangru Li <maxsli@protonmail.com>
2020-03-01 19:57:28 -0500

    Removing uncessary files

--- src/test/java/tests/VennSetTest.java
+++ src/test/java/tests/VennSetTest.java
@@ -1,36 +1,32 @@
-//package tests;
-//
-//import javafx.scene.control.TextField;
-//import javafx.scene.shape.Circle;
-//import models.Location;
-//import models.VennSet;
-//import models.VennShape;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class VennSetTest {
-//
-//	private VennSet vennSet;
-//	private VennShape vennShape;
-//	private Circle leftCircle;
-//	private Circle rightCircle;
-//
-//	@Before
-//	public void setUp() {
-//		this.leftCircle = new Circle();
-//		this.leftCircle.setCenterX(0);
-//		this.leftCircle.setCenterY(0);
-//		this.rightCircle = new Circle();
-//		this.rightCircle.setCenterX(50);
-//		this.rightCircle.setCenterY(50);
-//		this.vennShape = new VennShape(this.leftCircle, this.rightCircle);
-//		this.vennSet = new VennSet(this.vennShape);
-//	}
-//
-//	@Test
-//	public void test() {
-//
-//	}
-//}
+package tests;
+
+import javafx.scene.shape.Circle;
+import models.VennSet;
+import models.VennShape;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VennSetTest {
+
+	private VennSet vennSet;
+	private VennShape vennShape;
+	private Circle leftCircle;
+	private Circle rightCircle;
+
+	@BeforeEach
+	public void setUp() {
+		this.leftCircle = new Circle();
+		this.leftCircle.setCenterX(0);
+		this.leftCircle.setCenterY(0);
+		this.rightCircle = new Circle();
+		this.rightCircle.setCenterX(50);
+		this.rightCircle.setCenterY(50);
+		this.vennShape = new VennShape(this.leftCircle, this.rightCircle);
+		this.vennSet = new VennSet(this.vennShape);
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
--- src/test/java/tests/VennShapeTest.java
+++ src/test/java/tests/VennShapeTest.java
@@ -1,33 +1,31 @@
-//package tests;
-//
-//import javafx.scene.shape.Circle;
-//import models.VennShape;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class VennShapeTest {
-//
-//	private VennShape vennShape;
-//	private Circle leftCircle;
-//	private Circle rightCircle;
-//
-//	@Before
-//	public void setUp() {
-//		this.leftCircle = new Circle();
-//		this.rightCircle = new Circle();
-//		this.vennShape = new VennShape(this.leftCircle, this.rightCircle);
-//	}
-//
-//	@Test
-//	public void testLeftCircle() {
-//		assertEquals(this.vennShape.getLeftShape(), this.leftCircle);
-//	}
-//
-//	@Test
-//	public void testRightCircle() {
-//		assertEquals(this.vennShape.getRightShape(), this.rightCircle);
-//	}
-//
-//}
+package tests;
+
+import javafx.scene.shape.Circle;
+import models.VennShape;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VennShapeTest {
+
+	private VennShape vennShape;
+	private Circle leftCircle;
+	private Circle rightCircle;
+
+	@BeforeEach
+	public void setUp() {
+		this.leftCircle = new Circle();
+		this.rightCircle = new Circle();
+		this.vennShape = new VennShape(this.leftCircle, this.rightCircle);
+	}
+
+	@Test
+	public void testLeftCircle() {
+		assertEquals(this.vennShape.getLeftShape(), this.leftCircle);
+	}
+
+	@Test
+	public void testRightCircle() {
+		assertEquals(this.vennShape.getRightShape(), this.rightCircle);
+	}
+
+}
